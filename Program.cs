cce0cb8a07276bd51766b7a098257ecec75abb2a
Michael Hansen <gearsofwarfan97@gmail.com>
2020-02-28 22:52:29 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Methods_Project
+{
+    class Program
+    {
+        static void PrintMessage(string work, int X, int Y)
+        {
+            bool divideByZeroError = false;
+
+            if (Y == 0)
+            {
+                divideByZeroError = true;
+            }
+
+            static double basicCalucation(double x, double y, int func);
+
+
+        }
+         
+            
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Michael Hansen");
+
+            int X;
+            int Y;
+            string work;
+
+            Console.WriteLine("Please enter the first number of the equation");
+
+            while (!int.TryParse(Console.ReadLine(), out X))
+            {
+                Console.Clear();
+                Console.WriteLine("Please enter numbers only!");
+            }
+
+            Console.WriteLine("The first number you entered is:" + X);
+            Console.WriteLine("Please enter the second number of the equation");
+
+            while (!int.TryParse(Console.ReadLine(), out Y))
+            {
+                Console.Clear();
+                Console.WriteLine("Please enter numbers only!");
+            }
+
+            Console.WriteLine(" The second number you entered is:" + Y);
+
+            Console.WriteLine(" Please enter an addition, subtraction, division, or multiplication sign");
+
+            work = Console.ReadLine();
+
+            basicCalculation(X, Y, work);
+        }
+    }
+
